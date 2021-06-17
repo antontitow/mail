8d30e7b480275af5120e40884ec0b65e455fde48
antontitow@bk.ru <!Qaztit87>
2021-06-17 16:48:53 +0300

    start

--- src/main/java/ru/kkb/service/mail/verification/Application.java
+++ src/main/java/ru/kkb/service/mail/verification/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 @ConfigurationPropertiesScan("ru.kkb.service.mail.verification.config")
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
